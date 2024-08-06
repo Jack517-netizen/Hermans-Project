efa5dc7f6aad8afbeb3733d11d0913f84d9ac0ac
Jack517-netizen <kodjojosueayitey@gmail.com>
2024-08-06 09:59:47 +0000

    Adding some views: WelcomePage, FilePage, HelpPage. Configure several routes.

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,21 +1,33 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import WelcomeView from '../views/WelcomeView.vue'
+import FileView from '@/views/FileView.vue'
+import HelpView from '@/views/HelpView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      name: 'welcome',
+      component: WelcomeView
     },
+
+    /**
+     * FILE RESOURCES URL
+     */
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      path: '/file',
+      name: 'file',
+      component: FileView
+    },
+
+    /**
+     * HELP RESOURCES URL
+     */
+    {
+      path: '/help',
+      name: 'help',
+      component: HelpView
     }
   ]
 })
---
+++ src/views/WelcomeView.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="wrapper">
+    <div class="info-box">
+      <h1>LDAPV</h1>
+      <h3>
+        <em>
+          <span class="letter">L</span>ogiciel de <span class="letter">D</span>étermination et
+          d'<span class="letter">A</span>ctualisation du <span class="letter">P</span>rix de
+          <span class="letter">V</span>ente dans le BTP</em
+        >
+      </h3>
+      <br /><br />
+
+      <p>Vous ne savez pas par où commencer ? Voyons-voir...</p>
+      <ul>
+        <li>
+          Naviguer à l'onglet <span class="onglet">Fichier</span> pour créer un nouveau projet ou
+          charger un existant.
+        </li>
+        <li>
+          Naviguer à l'onglet <span class="onglet">Aide</span> pour toute demande d'aide, nous
+          notifier à propos de l'outil, ou être juste en contact.
+        </li>
+      </ul>
+      <br /><br />
+
+      <em
+        ><small
+          >Ce projet a été réalisé par Hermans NAYIBOU dans le cadre de l'implémentation de son
+          projet de fin d'études.</small
+        ></em
+      >
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.letter {
+  color: #8900f9;
+  font-weight: bold;
+}
+
+.onglet {
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  padding: 5px 20px;
+}
+
+.wrapper {
+  display: grid;
+  place-items: center;
+}
+
+.info-box {
+  width: 50%;
+  margin: 50px auto;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  padding: 10px;
+}
+.info-box h1,
+.info-box h3,
+.info-box small {
+  display: block;
+  text-align: center;
+}
+</style>
